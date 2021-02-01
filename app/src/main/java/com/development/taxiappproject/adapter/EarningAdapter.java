5b36e70e3e474b91e9dd4c36d9cdf2ce2aafd4a7
abbasTrax <abbasjaffary33@gmail.com>
2021-02-01 13:59:12 +0430

    Earning finished

--- app/src/main/java/com/development/taxiappproject/adapter/EarningAdapter.java
+++ app/src/main/java/com/development/taxiappproject/adapter/EarningAdapter.java
@@ -9,26 +9,17 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.development.taxiappproject.R;
+import com.development.taxiappproject.model.MyEarningClass;
 import com.development.taxiappproject.model.MyRideClass;
 
 import java.util.List;
 
-public class EarningAdapter extends  RecyclerView.Adapter<EarningAdapter.MyViewHolder> {
+public class EarningAdapter extends RecyclerView.Adapter<EarningAdapter.MyViewHolder> {
 
-    List<MyRideClass> myRideList ;
+    List<MyEarningClass> myRideList;
     private Context context;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView title;
-
-        public MyViewHolder(View view) {
-            super(view);
-            //   imageView = (ImageView)view.findViewById(R.id.image);
-//            cardView = (CardView)view.findViewById(R.id.cardView);
-        }
-    }
-
-    public EarningAdapter(Context context, List<MyRideClass> myRideList) {
+    public EarningAdapter(Context context, List<MyEarningClass> myRideList) {
         this.context = context;
         this.myRideList = myRideList;
     }
@@ -43,11 +34,29 @@ public class EarningAdapter extends  RecyclerView.Adapter<EarningAdapter.MyViewH
 
     @Override
     public void onBindViewHolder(EarningAdapter.MyViewHolder holder, int position) {
-
+        holder.timeTxt.setText(myRideList.get(position).getTime());
+        holder.fareTxt.setText(myRideList.get(position).getFare());
+        holder.milesTxt.setText(myRideList.get(position).getMiles());
+        holder.paidTxt.setText(myRideList.get(position).getPaidTo());
     }
 
     @Override
     public int getItemCount() {
         return myRideList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView timeTxt, fareTxt, milesTxt, paidTxt;
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            timeTxt = view.findViewById(R.id.earningItem_time_txt);
+            fareTxt = view.findViewById(R.id.earningItem_fare_txt);
+            milesTxt = view.findViewById(R.id.earningItem_miles_txt);
+            paidTxt = view.findViewById(R.id.earningItem_payToAdmin_txt);
+            //   imageView = (ImageView)view.findViewById(R.id.image);
+//            cardView = (CardView)view.findViewById(R.id.cardView);
+        }
     }
 }
--- app/src/main/java/com/development/taxiappproject/model/MyRideClass.java
+++ app/src/main/java/com/development/taxiappproject/model/MyRideClass.java
@@ -1,13 +1,67 @@
 package com.development.taxiappproject.model;
 
 public class MyRideClass {
-    private String name;
+    private String dateRide;
+    private String priceRide;
+    private String distanceRide;
+    private String timeRide;
+    private String startLocationRide;
+    private String endLocationRide;
 
-    public String getName() {
-        return name;
+    public MyRideClass(String dateRide, String priceRide, String distanceRide, String timeRide, String startLocationRide, String endLocationRide) {
+        this.dateRide = dateRide;
+        this.priceRide = priceRide;
+        this.distanceRide = distanceRide;
+        this.timeRide = timeRide;
+        this.startLocationRide = startLocationRide;
+        this.endLocationRide = endLocationRide;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getDateRide() {
+        return dateRide;
+    }
+
+    public void setDateRide(String dateRide) {
+        this.dateRide = dateRide;
+    }
+
+    public String getEndLocationRide() {
+        return endLocationRide;
+    }
+
+    public void setEndLocationRide(String endLocationRide) {
+        this.endLocationRide = endLocationRide;
+    }
+
+    public String getStartLocationRide() {
+        return startLocationRide;
+    }
+
+    public void setStartLocationRide(String startLocationRide) {
+        this.startLocationRide = startLocationRide;
+    }
+
+    public String getTimeRide() {
+        return timeRide;
+    }
+
+    public void setTimeRide(String timeRide) {
+        this.timeRide = timeRide;
+    }
+
+    public String getDistanceRide() {
+        return distanceRide;
+    }
+
+    public void setDistanceRide(String distanceRide) {
+        this.distanceRide = distanceRide;
+    }
+
+    public String getPriceRide() {
+        return priceRide;
+    }
+
+    public void setPriceRide(String priceRide) {
+        this.priceRide = priceRide;
     }
 }
